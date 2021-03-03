d23762a32453490b35282f0848123abcf61a018c
PMetcalf <paulmetcalf@hotmail.com>
2021-03-03 16:23:58 +0000

    Work in progress.

--- Figures/figures.m
+++ Figures/figures.m
@@ -1,7 +1,14 @@
 % Working with Figures
 
 % Very basic plotting
-x = [-100:10:100]
-y = x.^2
+x = [-100:10:100];
+y = x.^2;
 
-plot(x, y)
+plot(x, y);
+
+% Adding Title, Labels, Grids
+x = [0:0.01:10];
+y = sin(x);
+
+% xlabel, ylabel, title, grid, squaring of axes
+plot(x, y), xlabel('x'), ylabel('Sin(x)'), title('Sin(x) Graph'), grid on, axis equal;
