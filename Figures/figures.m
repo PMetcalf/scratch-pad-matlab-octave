96bfc4c69c879a23e687ec0ac5b41f86ede3e4ac
Paul Metcalf <paulmetcalf@hotmail.com>
2021-03-05 15:23:33 +0000

    Work in progress.

--- Figures/figures.m
+++ Figures/figures.m
@@ -35,3 +35,11 @@ x = [0:0.01:10];
 y = exp(-x).*sin(2*x + 3);
 
 plot(x, y), axis([0 10 -1 1])
+
+% Generating Subplots
+% Syntax: subplot(m, n, p) where m/n = rows/columns and p = position
+x = [0:0.01:5];
+
+y = exp(-1.5*x).*sin(10*x);
+subplot(1,2,1)
+plot(x,y), xlabel('x'), ylabel('exp(-1.5x)*sin(10x)'), axis([0 5 -1 1])
